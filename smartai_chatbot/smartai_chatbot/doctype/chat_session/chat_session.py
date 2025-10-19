dd82fd90da5c27176500e30213172555b6d828f9
Majid <khan2614004@gmail.com>
2025-10-19 21:11:01 +0500

    Add ChatSession class to chat_session.py

--- smartai_chatbot/smartai_chatbot/doctype/chat_session/chat_session.py
+++ smartai_chatbot/smartai_chatbot/doctype/chat_session/chat_session.py
@@ -1,4 +1,8 @@
 # -*- coding: utf-8 -*-
-# This file is required for Frappe to load the doctype module.
 
 from __future__ import unicode_literals
+import frappe
+from frappe.model.document import Document
+
+class ChatSession(Document):
+    pass
